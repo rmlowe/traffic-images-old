d8482770c1c10ced9af2898ae3c42b6afc9eec5e
Robert Lowe <robertmlowe@rmlowe.com>
2018-02-04 19:16:25 +0800

    Lots of good stuff

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCB5AW2Acz2gAAiz4vCiuzLnFMrZ1ixOJs';
 
-// Create a new component.  This component should produce
-// some HTML
-const App = () => {
-   return (
-      <div>
-         <SearchBar />
-      </div>
-   );
+class App extends Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         videos: [],
+         selectedVideo: null
+      };
+
+      YTSearch({key: API_KEY, term: 'Minions'}, (videos) => {
+         this.setState({
+            videos: videos,
+            selectedVideo: videos[0]
+         });
+      });
+   }
+
+   render() {
+      return (
+         <div>
+            <SearchBar />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList
+               onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+               videos={this.state.videos} />
+         </div>
+      );
+   }
 }
 
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
